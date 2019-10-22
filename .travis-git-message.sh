cc60b34c42d78d924e33df5572cc5b295a61dd3a
Mark Raynsford <code@io7m.com>
2019-10-22 16:46:28 +0000

    Use correct application path in Travis CI

--- .travis-git-message.sh
+++ .travis-git-message.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 GP_GIT_COMMIT=$(git rev-list --max-count=1 HEAD) || exit 1
-SIMPLYE_VERSION=$(grep versionCode app/version.properties | sed 's/versionCode=//g') || exit 1
+SIMPLYE_VERSION=$(grep versionCode simplified-app-openebooks/version.properties | sed 's/versionCode=//g') || exit 1
 
 cat <<EOF
 Build of ${GP_GIT_COMMIT}
